4089bd36900c3c17247c105f31bc9d6c5c783864
uladaharanina <uladaharanina@gmail.com>
2024-10-01 11:12:35 -0400

    feat: add lecture

---
+++ week1/FizzBuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿/**
+integer n, for every i <=n
+if i % 3 == 0 then (Fizz)
+if i % 5 == 0 then (Buzz)
+if i % 5 and i % 3 then (FizzBuzz)
+*/
+
+
+
+//  namespace is like a Folder that orginizes your files, 
+//different namespaces can help you to have the same filenames and don't mix it up
+namespace FizzBuzz;
+
+class Program{
+
+    //Entry Point, dotnet looks for this method
+    static void Main(string[] args){
+
+        FizzBuzz(14);
+
+    }
+
+
+    //Function to print FizzBuzz numbers
+
+    //Public access modifer (access to all classes)
+    //Private (restricted to only the same class it declared in)
+    //Protected (accessed to itself and subclasses only)
+    public static void FizzBuzz(int n){
+        
+        for(int i = 1; i < n; i++){
+
+            if( i % 15 == 0){
+                Console.WriteLine("FizzBuzz");
+            }
+
+            else if( i % 3 == 0){
+                Console.WriteLine("Fizz");
+            }
+
+            else if( i % 5 == 0){
+                Console.WriteLine("Buzz");
+            }
+            else{
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
+
+
---
+++ week1/MagicBall/MagicBall.cs
@@ -0,0 +1,46 @@
+﻿class MagicBall{
+
+    public static void Main(string[] args){
+        //run our function here
+
+        Console.WriteLine("Welcome to the MagicBall");
+        Console.WriteLine("Ask your question and I'll help you find the answer");
+
+        string? userInput = Console.ReadLine();
+
+        while(string.IsNullOrEmpty(userInput)){
+            Console.WriteLine("Not valid user input");
+            userInput = Console.ReadLine();
+        }
+        
+            string? answer = GetMagicAnswer();
+            Console.WriteLine($"The Magic Ball says: {answer}");
+        
+    }
+
+
+    /*
+        Select and return the answer
+    */
+
+    public static string GetMagicAnswer(){
+
+        string[] predictions = {
+            "You will receive everything you want!",
+            "You will have a great day ahead!",
+            "You will find a new friend!",
+            "You will have a happy life!",
+            "You will be rich and famous!",
+            "You will be able to achieve great things!",
+            "You will feel happy and fulfilled!"
+        };
+
+        // Create a new instance of the Random class
+        Random numberGenerator = new Random();
+        int randomIndex = numberGenerator.Next(predictions.Length);
+
+        return predictions[randomIndex];
+
+    }
+
+}
